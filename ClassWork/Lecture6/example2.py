518d3623426a8b1be329b682d1aaf2df835bbbe7
aseelrahman <132919956+aseelrahman@users.noreply.github.com>
2025-03-25 17:44:53 +0000

    Completed Lecture 6 – Assignment 2

---
+++ ClassWork/Lecture6/example2.py
@@ -0,0 +1,12 @@
+# Adding New Key-Value Pairs
+
+# Create a dictionary with two key/pairs.
+my_dictionary = {'color': 'green', 'points': 5}
+
+# Print out the entire dictionary
+print(my_dictionary)
+
+# Two additional key pairs to the dictionary.
+my_dictionary['first_name'] = "Tom"
+my_dictionary['last_name'] = 'Arnold'
+print(my_dictionary)
