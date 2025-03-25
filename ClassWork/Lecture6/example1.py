8d384ff028d28c5cebd9384031e5e4b4708bcbca
aseelrahman <132919956+aseelrahman@users.noreply.github.com>
2025-03-25 17:37:55 +0000

    Completed Lecture 6 – Assignment 1

---
+++ ClassWork/Lecture6/example1.py
@@ -0,0 +1,11 @@
+# Accessing Values
+
+# Create a dictionary with two key/value pairs.
+my_dictionary = {'color': 'green', 'points': 5}
+
+# Assign the value 5 to points variable.
+points = my_dictionary["points"]
+
+# Format the output using the F-String
+print(f"You've just earned {points} points!")
+print(f"Do you like the color {my_dictionary['color']}?")
